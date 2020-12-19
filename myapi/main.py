5245a9ba070ee3cbbfb747759d9ddf15aef7c189
Igor Brigadir <igor.brigadir@gmail.com>
2020-12-19 15:02:50 +0000

    add api template draft

---
+++ myapi/main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+
+def main():
+    print("Hello World")
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True, workers=2)
+
+
+if __name__ == "__main__":
+    main()
