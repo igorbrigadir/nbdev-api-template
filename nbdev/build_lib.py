901e56cda6a80b76b53528a650b2e303a74cfda0
Igor Brigadir <igor.brigadir@gmail.com>
2020-12-22 20:36:14 +0000

    rebuild lib

--- nbdev/build_lib.py
+++ nbdev/build_lib.py
@@ -3,4 +3,11 @@
 
 import nbdev
 
+# from nbdev.export import notebook2script
+# import fastcore.foundation
+# from functools import lru_cache
+
 print(nbdev.__version__)
+
+
+# notebook2script(fname="test_nbdev.ipynb")
